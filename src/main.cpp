e56f2b0d35754a890d97783774a574458a0dc384
Dominik <zawlockidominik@gmail.com>
2025-10-22 01:10:19 +0200

    Loading csv file datasheet

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <sstream>
 
 #include "classifiers/KNN.h"
 
@@ -8,17 +10,57 @@ using namespace std;
 using namespace cll;
 
 
-int main() {
-    vector<vector<float>> X = {
-        vector<float>{1, 1},
-        vector<float>{2, 2},
-        vector<float>{3, 3},
-        vector<float>{4, 4},
-        vector<float>{4.5, 4},
-        vector<float>{5, 5},
-    };
+vector<vector<string>> readCSV(const string& filename, int skip_rows=0) {
+    vector<vector<string>> data;
+    ifstream file(filename);
 
-    vector<int> Y = {1, 2, 3, 4, 4, 5,};
+    if (!file.is_open()) {
+        cerr << "Failed to open file: " << filename << endl;
+        return data;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        if (skip_rows-- > 0) {
+            continue;
+        }
+
+        vector<string> row;
+        stringstream ss(line);
+        string cell;
+
+        while (getline(ss, cell, ',')) {
+            row.push_back(cell);
+        }
+
+        if (!row.empty()) {
+            data.push_back(row);
+        }
+    }
+
+    file.close();
+    return data;
+}
+
+
+int main() {
+    auto bmi_data = readCSV("data/bmi_simplified.csv", 1);
+
+    map<string, int> class_to_int{{"Underweight", 1}, {"Normal Weight", 2}, {"Overweight", 3}, {"Obese", 4}};
+    map<int, string> int_to_class{{1, "Underweight"}, {2, "Normal Weight"}, {3, "Overweight"}, {4, "Obese"}};
+
+    vector<vector<float>> X(bmi_data.size());
+
+    for (int i = 0; i < bmi_data.size(); i++) {
+        X[i] = vector<float>(bmi_data[i].size() - 1);
+        X[i][0] = stof(bmi_data[i][0]);
+        X[i][1] = stof(bmi_data[i][1]);
+    }
+
+    vector<int> Y(bmi_data.size());
+    for (int i = 0; i < bmi_data.size(); i++) {
+        Y[i] = class_to_int[bmi_data[i][2]];
+    }
 
     KNN model(3);
     model.fit(X, Y);
