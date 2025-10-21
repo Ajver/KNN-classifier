625743317868525a005f570e198af55246791930
Dominik <zawlockidominik@gmail.com>
2025-10-22 00:39:02 +0200

    Add sample data and plotting via python's matplotlib

---
+++ py_plotting/plot_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv("../data/bmi_simplified.csv")
+print(data)
+
+classes = data["BmiClass"].unique()
+class_to_color = {cls: idx for idx, cls in enumerate(classes)}
+colors = data["BmiClass"].map(class_to_color)
+
+print(classes)
+
+ax = plt.subplots()[1]
+scatter = ax.scatter(data["Weight"], data["Height"], c=colors, cmap='viridis')
+ax.legend(scatter.legend_elements()[0], classes)
+plt.show()
+
