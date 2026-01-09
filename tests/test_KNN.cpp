c577cab05632d32964ad7127b7298a63eccf57ea
Dominik <zawlockidominik@gmail.com>
2026-01-09 15:38:17 +0100

    Create unit tests for KNN classifier

---
+++ tests/test_KNN.cpp
@@ -0,0 +1,128 @@
+#include <gtest/gtest.h>
+
+#include "../src/classifiers/KNN.h"
+
+
+TEST(KNNTest, PredictK_1) {
+    std::vector<std::vector<float>> X {
+        {0, 0, 0},
+        {0, 1, 0},
+        {0, 0, 2},
+        {-3, 0, 1},
+    };
+
+    std::vector<int> y {
+        1,
+        2,
+        3,
+        4,
+    };
+
+    cll::KNN knn(1);
+    knn.fit(X, y);
+
+    EXPECT_EQ(knn.predict({0, 0.4, 0}).predicted_class, 1);
+    EXPECT_EQ(knn.predict({0, 0.3, 0.4}).predicted_class, 1);
+    EXPECT_EQ(knn.predict({0, -1, 0}).predicted_class, 1);
+
+    EXPECT_EQ(knn.predict({0, 1.0, 0}).predicted_class, 2);
+    EXPECT_EQ(knn.predict({0, 0.6, 0}).predicted_class, 2);
+
+    EXPECT_EQ(knn.predict({0, 0.5, 1.3}).predicted_class, 3);
+
+    EXPECT_EQ(knn.predict({-2, 0.1, 0.1}).predicted_class, 4);
+}
+
+TEST(KNNTest, PredictK_3) {
+    std::vector<std::vector<float>> X {
+        {0.0, 0.0, 0.0},
+        {0.1, 0.0, 0.0},
+        {0.0, 0.1, 0.0},
+        {5.0, 5.0, 5.0},
+        {5.1, 5.0, 5.0},
+        {5.0, 5.1, 5.0},
+        {10.0, 10.0, 10.0},
+        {10.1, 10.0, 10.0},
+        {10.0, 10.1, 10.0},
+        {0.0, 0.0, 10.0}
+    };
+
+    std::vector<int> y {1, 1, 1, 2, 2, 2, 3, 3, 3, 4};
+
+    cll::KNN knn(3);
+    knn.fit(X, y);
+
+    EXPECT_EQ(knn.predict({0.05, 0.05, 0.0}).predicted_class, 1);
+    EXPECT_EQ(knn.predict({5.05, 5.05, 5.0}).predicted_class, 2);
+    EXPECT_EQ(knn.predict({10.05, 10.05, 10.0}).predicted_class, 3);
+    EXPECT_EQ(knn.predict({0.0, 0.0, 5.0}).predicted_class, 1);
+}
+
+
+TEST(KNNTest, PredictProbability) {
+    /*
+
+      1         2
+
+      A         B
+
+      1         2
+
+     */
+    std::vector<std::vector<float>> X {
+        {0.0, 0.0},
+        {1.0, 1.0},
+        {0.0, 1.0},
+        {1.0, 0.0},
+    };
+
+    std::vector<int> y {1, 2, 1, 2};
+
+    cll::KNN knn(3);
+    knn.fit(X, y);
+
+    // A
+    EXPECT_EQ(knn.predict({0.0, 0.5}).predicted_class, 1);
+    EXPECT_NEAR(knn.predict({0.0, 0.5}).confidence, 2./3, 1e-5);
+
+    // B
+    EXPECT_EQ(knn.predict({1.0, 0.5}).predicted_class, 2);
+    EXPECT_NEAR(knn.predict({1.0, 0.5}).confidence, 2./3, 1e-5);
+}
+
+
+TEST(KNNTest, PredictMultipleK_1) {
+    std::vector<std::vector<float>> X {
+            {0, 0, 0},
+            {0, 1, 0},
+            {0, 0, 2},
+            {-3, 0, 1},
+        };
+
+    std::vector<int> y {
+        1,
+        2,
+        3,
+        4,
+    };
+
+    cll::KNN knn(1);
+    knn.fit(X, y);
+
+    std::vector<std::vector<float>> Xtest {
+        {0, 0.4, 0},
+        {0, -1, 0},
+        {0, 1.0, 0},
+        {0, 0.6, 0},
+        {0, 0.5, 1.3},
+        {-2, 0.1, 0.1},
+    };
+    std::vector<cll::ClassificationResult> results = knn.predict(Xtest);
+
+    EXPECT_EQ(results[0].predicted_class, 1);
+    EXPECT_EQ(results[1].predicted_class, 1);
+    EXPECT_EQ(results[2].predicted_class, 2);
+    EXPECT_EQ(results[3].predicted_class, 2);
+    EXPECT_EQ(results[4].predicted_class, 3);
+    EXPECT_EQ(results[5].predicted_class, 4);
+}
