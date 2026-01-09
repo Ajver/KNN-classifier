3992d36e58ea2e2cecbb135bb31ed62591aba0ab
Dominik <zawlockidominik@gmail.com>
2026-01-09 16:15:41 +0100

    Test NoScaler vs NormalScaler

--- tests/test_KNN.cpp
+++ tests/test_KNN.cpp
@@ -59,7 +59,7 @@ TEST(KNNTest, PredictK_3) {
 }
 
 
-TEST(KNNTest, PredictProbability) {
+TEST(KNNTest, PredictConfidenceAndClassCounts) {
     /*
 
       1         2
@@ -81,23 +81,27 @@ TEST(KNNTest, PredictProbability) {
     cll::KNN knn(3);
     knn.fit(X, y);
 
-    // A
-    EXPECT_EQ(knn.predict({0.0, 0.5}).predicted_class, 1);
-    EXPECT_NEAR(knn.predict({0.0, 0.5}).confidence, 2./3, 1e-5);
+    std::vector<float> A {0.0, 0.5};
+    EXPECT_EQ(knn.predict(A).predicted_class, 1);
+    EXPECT_NEAR(knn.predict(A).confidence, 2./3, 1e-5);
+    EXPECT_EQ(knn.predict(A).classes[1], 2);
+    EXPECT_EQ(knn.predict(A).classes[2], 1);
 
-    // B
-    EXPECT_EQ(knn.predict({1.0, 0.5}).predicted_class, 2);
-    EXPECT_NEAR(knn.predict({1.0, 0.5}).confidence, 2./3, 1e-5);
+    std::vector<float> B {1.0, 0.5};
+    EXPECT_EQ(knn.predict(B).predicted_class, 2);
+    EXPECT_NEAR(knn.predict(B).confidence, 2./3, 1e-5);
+    EXPECT_EQ(knn.predict(B).classes[1], 1);
+    EXPECT_EQ(knn.predict(B).classes[2], 2);
 }
 
 
 TEST(KNNTest, PredictMultipleK_1) {
     std::vector<std::vector<float>> X {
-            {0, 0, 0},
-            {0, 1, 0},
-            {0, 0, 2},
-            {-3, 0, 1},
-        };
+        {0, 0, 0},
+        {0, 1, 0},
+        {0, 0, 2},
+        {-3, 0, 1},
+    };
 
     std::vector<int> y {
         1,
@@ -125,4 +129,46 @@ TEST(KNNTest, PredictMultipleK_1) {
     EXPECT_EQ(results[3].predicted_class, 2);
     EXPECT_EQ(results[4].predicted_class, 3);
     EXPECT_EQ(results[5].predicted_class, 4);
+}
+
+TEST(KNNTest, NormalScaler) {
+    /*
+        - Big difference in X axis
+        - Small difference in Y axis
+
+    Before normalisation, A is closer to 2...
+
+                                        A
+                        1
+
+      1                                     2
+
+                        2
+
+    But after normalisation, A is closer to 1...
+
+           A
+         1
+      1     2
+         2
+
+    */
+    std::vector<std::vector<float>> X {
+            {0, 1},
+            {10, 0},
+            {20, 1},
+            {10, 2},
+        };
+
+    std::vector<int> y {1, 2, 2, 1};
+
+    cll::KNN knn_no_scaler(1, cll::NONE);
+    cll::KNN knn_normal_scaler(1, cll::NORMALIZE);
+    knn_no_scaler.fit(X, y);
+    knn_normal_scaler.fit(X, y);
+
+    std::vector<float> A = {18.0, 2.3};
+    EXPECT_EQ(knn_no_scaler.predict(A).predicted_class, 2);
+
+    EXPECT_EQ(knn_normal_scaler.predict(A).predicted_class, 1);
 }
